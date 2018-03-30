5d58b2a0a0a5187f7bf600c1083dfa3e2d7c3b35
William Barnes <barnesw91291@gmail.com>
2018-03-30 10:07:38 -0700

    images download correctly now

--- FileSaver.js
+++ FileSaver.js
@@ -1,9 +1,16 @@
 const CreateFileSaver = (axios, writeFile) => {
   return {
     saveImage: (imageUri, folder, filename, extention) => {
-      return axios.get(imageUri)
+      return axios.request({
+        responseType: 'arraybuffer',
+        url: imageUri,
+        method: 'get',
+        headers: {
+          'Content-Type': 'image/png',
+        },
+      })
         .then(function (response) {
-          return writeFile(`./${folder}/${filename}.${extention}`, response.data, 'binary');
+          return writeFile(`./${folder}/${filename}.${extention}`, response.data);
         });
     }
   };
